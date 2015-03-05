a3c0d17a54f7744b1d6b50252d6ff2befade14ca
Ben Morris <github@benmorris.net>
2015-03-04 22:20:10 -0500

    fix find method

--- app/models/service_cache.rb
+++ app/models/service_cache.rb
@@ -1,11 +1,7 @@
 class ServiceCache < ActiveRecord::Base
 
-  def self.all_records
-    ServiceCache.where(service:self.new.class.to_s)
-  end
-
   def self.find(key)
-    all_records.where(key:key)
+    ServiceCache.where(service:self.new.class.to_s, key:key).first.data
   end
 
 private
@@ -14,6 +10,5 @@ private
     cache = ServiceCache.find_or_initialize_by(service:self.new.class.to_s,key:key)
     cache.update(data:data)
   end
-
   
 end
