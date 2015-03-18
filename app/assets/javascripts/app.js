94a1ef68388e57b43d6730815732b914322560c0
Jeffrey M Farley <JeffreyMFarley@users.noreply.github.com>
2015-03-18 15:59:40 -0400

    Support for Google Charts
    1. Added asynchronous loading so that script=Google does not block
    rendering

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -29,3 +29,49 @@ templicious.config([
 ]);
 
 var controllers = angular.module('controllers', []);
+
+templicious
+.factory('ChartInitializer', function ($rootScope, $window, $q) {
+    var deferred = $q.defer();
+
+    $window.google.load('visualization', '1',
+        {
+            packages: ['corechart'],
+            callback: function () {
+                $rootScope.$apply(function () {
+                    deferred.resolve();
+                });
+            }
+        });
+
+    //Usage: ChartInitializer.initialized.then(callback)
+    return {
+        initialized: deferred.promise
+    };
+});
+
+templicious
+.factory('MapInitializer', function ($rootScope, $window, $q) {
+    //Google's url for async maps initialization accepting callback function
+    var asyncUrl = 'https://maps.googleapis.com/maps/api/js?callback=',
+        deferred = $q.defer();
+
+    //Callback function - resolving promise after maps successfully loaded
+    $window.googleMapsInitialized = deferred.resolve;
+
+    //Async loader
+    var asyncLoad = function (asyncUrl, callbackName) {
+        var script = document.createElement('script');
+        //script.type = 'text/javascript';
+        script.src = asyncUrl + callbackName;
+        document.body.appendChild(script);
+    };
+
+    //Start loading google maps
+    asyncLoad(asyncUrl, 'googleMapsInitialized');
+
+    //Usage: MapInitializer.initialized.then(callback)
+    return {
+        initialized: deferred.promise
+    };
+});
